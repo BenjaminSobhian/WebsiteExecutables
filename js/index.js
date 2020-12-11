a6b804cedf29d8fcacaba1209aa219c084bcd61d
SommerOliver <oliver.sommer2005@gmail.com>
2020-12-11 23:15:31 +0100

    Bootstrap navbar, dropdown

--- js/index.js
+++ js/index.js
@@ -1,19 +1,17 @@
-$(document).ready(function() {
-    $(".menu-icon").on("click", function() {
-          $("nav ul").toggleClass("showing");
+$(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
     });
 });
 
 // Scrolling Effect
 
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('navbar_black');
-		$('nav').removeClass('navbar_transparent');
-    }
-
-    else {
-          $('nav').addClass('navbar_transparent');
-		$('nav').removeClass('navbar_black');
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('navbar_black');
+        $('nav').removeClass('navbar_transparent');
+    } else {
+        $('nav').addClass('navbar_transparent');
+        $('nav').removeClass('navbar_black');
     }
 })
