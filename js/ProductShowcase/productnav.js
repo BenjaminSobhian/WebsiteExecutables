0c01a8a127d162139e77873c013508352e3e9859
benisobhian@gmail.com <S4a5jq%%?X>
2021-01-02 20:53:01 +0100

    Finished first laptop (ProX) desktop view.

--- js/ProductShowcase/productnav.js
+++ js/ProductShowcase/productnav.js
@@ -3,9 +3,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.querySelector('.navbar').style.top = "0";
+    document.getElementById('#navbar').style.top = "0";
   } else {
-    document.querySelector('.navbar').style.top = "-100px";
+    document.getElementById('#navbar').style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
 }
